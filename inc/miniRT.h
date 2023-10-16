ffbd855843757daa6288faf27da9371d85a4cbad
Sirui Qiu <sqiu@c2r2p2.42vienna.com>
2023-10-16 15:05:49 +0200

    feat: add main.c and miniRT.h

---
+++ inc/miniRT.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   miniRT.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/16 14:58:45 by sqiu              #+#    #+#             */
+/*   Updated: 2023/10/16 15:03:26 by sqiu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINIRT_H
+# define MINIRT_H
+
+/* ====== LIBRARIES ====== */
+
+# include <stdio.h>
+# include <fcntl.h>				// req for open()
+# include <unistd.h>			// req for read(), write(), close()
+# include <stdlib.h>
+
+
+#endif
