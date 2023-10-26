19e8ef64248867fd6dbcf5aedcfb23be4dea7785
Sirui <sirui.qiu01@gmail.com>
2023-10-26 15:14:34 +0200

    fix: rm consecutive newline

--- inc/miniRT.h
+++ inc/miniRT.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   miniRT.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
+/*   By: u413q <u413q@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 14:58:45 by sqiu              #+#    #+#             */
-/*   Updated: 2023/10/16 16:31:13 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/10/26 15:14:07 by u413q            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,5 @@
 # include <unistd.h>			// req for read(), write(), close()
 # include <stdlib.h>
 # include "ft_print.h"
-
 
 #endif
