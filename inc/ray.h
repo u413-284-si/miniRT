2e80f793337d3624cac2aae302da701dfad0dcf7
Sirui <sirui.qiu01@gmail.com>
2023-10-27 15:31:00 +0200

    feat: add ray struct & ray_colour function

--- inc/ray.h
+++ inc/ray.h
@@ -6,7 +6,7 @@
 /*   By: u413q <u413q@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 12:02:44 by u413q             #+#    #+#             */
-/*   Updated: 2023/10/27 12:05:07 by u413q            ###   ########.fr       */
+/*   Updated: 2023/10/27 15:30:08 by u413q            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,41 @@
 /* ====== LIBRARIES ====== */
 
 # include "vec3.h"
+# include "colour.h"
+
+/* ====== DEFINITIONS ====== */
+
+/**
+ * @brief Represents a ray
+ * @param origin	point of ray origin
+ * @param direction	vector of ray direction
+ * @param d			distance into ray direction
+ */
+typedef struct s_ray
+{
+	t_vec3	origin;
+	t_vec3	direction;
+	float	d;
+}	t_ray;
 
 /* ====== DECLARATIONS ====== */
 
-t_vec3	ft_ray(t_vec3 origin, t_vec3 direction);
+/**
+ * @brief 
+ * 
+ * @param origin 
+ * @param direction 
+ * @param d 
+ * @return t_vec3 
+ */
+t_vec3		ft_ray(t_vec3 origin, t_vec3 direction, float d);
+
+/**
+ * @brief Returns the colour of a ray
+ * 
+ * @param ray Ray sent into the scene
+ * @return t_colour Ray colour
+ */
+t_colour	ft_ray_colour(t_ray ray);
 
 #endif
--- src/ray.c
+++ src/ray.c
@@ -6,7 +6,7 @@
 /*   By: u413q <u413q@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 12:05:20 by u413q             #+#    #+#             */
-/*   Updated: 2023/10/27 12:08:35 by u413q            ###   ########.fr       */
+/*   Updated: 2023/10/27 15:27:07 by u413q            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,26 @@
 t_vec3	ft_ray(t_vec3 origin, t_vec3 direction, float d)
 {
 	return (ft_vec3_add(origin, ft_vec3_scale(direction, d)));
+}
+
+t_colour	ft_ray_colour(t_ray ray)
+{
+	t_colour	ray_colour;
+	t_colour	white;
+	t_colour	blue;
+	t_vec3		unit_direction;
+	float		a;
+
+	white.r = 1.0;
+	white.g = 1.0;
+	white.b = 1.0;
+	blue.r = 0.5;
+	blue.g = 0.7;
+	blue.b = 1.0;
+	unit_direction = ft_vec3_norm(ray.direction);
+	a = 0.5 * (unit_direction.y + 1.0);
+	ray_colour.r = (1.0 - a) * white.r + a * blue.r;
+	ray_colour.g = (1.0 - a) * white.g + a * blue.g;
+	ray_colour.b = (1.0 - a) * white.b + a * blue.b;
+	return (ray_colour);
 }
