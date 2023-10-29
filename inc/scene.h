20e5b702f60548ab46ae6d25ff41b38fc0663ea5
Sirui <sirui.qiu01@gmail.com>
2023-10-29 12:22:48 +0100

    feat: add plane, cylinder definitions

--- inc/scene.h
+++ inc/scene.h
@@ -6,7 +6,7 @@
 /*   By: u413q <u413q@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 17:02:07 by u413q             #+#    #+#             */
-/*   Updated: 2023/10/29 11:38:19 by u413q            ###   ########.fr       */
+/*   Updated: 2023/10/29 12:21:34 by u413q            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,21 +27,56 @@
  * @brief Represents a sphere
  * @param centre	Centre of the sphere
  * @param r			Radius of the sphere
+ * @param colour	Colour of the sphere
  */
 typedef struct s_sphere
 {
-	t_vec3	centre;
-	float	r;
+	t_vec3		centre;
+	float		r;
+	t_colour	colour;
 }	t_sphere;
+
+/**
+ * @brief Represents a plane
+ * @param point		Point on the plane
+ * @param normal	Normalised [-1, 1] normal vector of the plane
+ * @param colour	Colour of the plane
+ */
+typedef struct s_plane
+{
+	t_vec3		point;
+	t_vec3		normal;
+	t_colour	colour;
+}	t_sphere;
+
+/**
+ * @brief Represents a cylinder
+ * @param centre	Centre of the cylinder
+ * @param axis		Normalised [-1, 1] axis of the cylinder
+ * @param d			Diameter of the cylinder
+ * @param h			Height of the cylinder
+ * @param colour	Colour of the cylinder
+ */
+typedef struct s_cylinder
+{
+	t_vec3		centre;
+	t_vec3		axis;
+	float		d;
+	float		h;
+	t_colour	colour;
+}	t_cylinder;
 
 /**
  * @brief Contains all entities in the scene
  * @param sp		Array of spheres
- * @param 
+ * @param pl		Array of planes
+ * @param cy		Array of cylinders
  */
 typedef struct s_entities
 {
 	t_sphere	*sp;
+	t_plane		*pl;
+	t_cylinder	*cy;
 }	t_entities;
 
 /* ====== DECLARATIONS ====== */
@@ -66,5 +101,12 @@ typedef struct s_entities
  */
 bool	ft_hit_sphere(t_sphere sp, t_ray ray, t_hitrecord *rec, \
 	t_interval ray_d);
+
+/**
+ * @brief Builds the scene by filling it with entities
+ * 
+ * @param scene 	Struct with all entities
+ */
+void	ft_create_scene(t_entities *scene);
 
 #endif
