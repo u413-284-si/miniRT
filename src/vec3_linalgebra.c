b11a69273ed6c0a13d409b5a1b5d14660a013687
Sirui <sirui.qiu01@gmail.com>
2023-10-31 17:50:40 +0100

    refactor: return compound literals

--- src/vec3_linalgebra.c
+++ src/vec3_linalgebra.c
@@ -6,7 +6,7 @@
 /*   By: u413q <u413q@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 14:52:25 by u413q             #+#    #+#             */
-/*   Updated: 2023/10/31 11:26:22 by u413q            ###   ########.fr       */
+/*   Updated: 2023/10/31 17:49:16 by u413q            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,10 +32,9 @@ float	ft_vec3_angle(t_vec3 v1, t_vec3 v2)
 
 t_vec3	ft_vec3_hadamard(t_vec3 v1, t_vec3 v2)
 {
-	t_vec3	rtrn;
-
-	rtrn.x = v1.x * v2.x;
-	rtrn.y = v1.y * v2.y;
-	rtrn.z = v1.z * v2.z;
-	return (rtrn);
+	return ((t_vec3){
+		.x = v1.x * v2.x,
+		.y = v1.y * v2.y,
+		.z = v1.z * v2.z,
+	});
 }
