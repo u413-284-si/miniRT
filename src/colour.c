ec69f932f9fa51853185089b019bbd3df383745f
Sirui <sirui.qiu01@gmail.com>
2023-10-31 19:23:53 +0100

    feat: add colour arithmetics

--- src/colour.c
+++ src/colour.c
@@ -6,11 +6,11 @@
 /*   By: u413q <u413q@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 11:30:57 by u413q             #+#    #+#             */
-/*   Updated: 2023/10/27 11:33:08 by u413q            ###   ########.fr       */
+/*   Updated: 2023/10/31 17:53:06 by u413q            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "colour.h"
+#include "colour.h"
 
 void	ft_write_colour(t_colour pixel)
 {
@@ -22,4 +22,22 @@ void	ft_write_colour(t_colour pixel)
 	ig = (int)(255.99 * pixel.g);
 	ib = (int)(255.99 * pixel.b);
 	printf("%d %d %d\n", ir, ig, ib);
+}
+
+t_colour	ft_add_colour(t_colour c1, t_colour c2)
+{
+	return ((t_colour){
+		.r = c1.r + c2.r,
+		.b = c1.b + c2.b,
+		.g = c1.g + c2.g,
+	});
+}
+
+t_colour	ft_hadamard_colour(t_colour c1, t_colour c2)
+{
+	return ((t_colour){
+		.r = c1.r * c2.r,
+		.b = c1.b * c2.b,
+		.g = c1.g * c2.g,
+	});
 }
