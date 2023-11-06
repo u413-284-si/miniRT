24647868c9e13d287eac1863cf7f91bf0d7e8426
Sirui Qiu <sqiu@c2r2p2.42vienna.com>
2023-11-06 15:39:39 +0100

    refactor: call hit_detection according to type

--- src/hit.c
+++ src/hit.c
@@ -6,14 +6,21 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 12:32:23 by sqiu              #+#    #+#             */
-/*   Updated: 2023/11/03 12:36:15 by sqiu             ###   ########.fr       */
+/*   Updated: 2023/11/06 15:25:41 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "hit_manager.h"
 
-bool	ft_hit_scene(t_entities scene, t_ray ray, t_hitrecord *rec, \
+bool	ft_hit_scene(t_hittable obj, t_ray ray, t_hitrecord *rec, \
 	t_interval ray_d)
 {
-	return (ft_hit_scene_sp(scene, ray, rec, ray_d));
+	if (obj.type == SPHERE)
+		return (ft_hit_sphere(obj.params.sp, ray, rec, ray_d));
+	else if (obj.type == PLANE)
+		return (ft_hit_plane(obj.params.pl, ray, rec, ray_d));
+/* 	else if (obj.type == CYLINDER)
+		return (ft_hit_cylinder(obj.params.cy, ray, rec, ray_d)); */
+	else
+		return (false);
 }
