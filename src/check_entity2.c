36085e208061d1daf0dec6d81324c8af2e2c1e45
Gabriel <gwolf@student.42vienna.com>
2023-11-10 19:32:09 +0100

    fix: add checker functions for all entities

--- src/check_entity2.c
+++ src/check_entity2.c
@@ -1,19 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   check_entity1.c                                    :+:      :+:    :+:   */
+/*   check_entity2.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 18:17:46 by gwolf             #+#    #+#             */
-/*   Updated: 2023/11/10 19:28:23 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/11/10 19:31:09 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "check.h"
 
-bool	ft_check_ambient(char *line)
+bool	ft_check_sphere(char *line)
 {
+	ft_skip_space(&line);
+	if (!ft_is_valid_float_block(&line))
+		return (false);
 	ft_skip_space(&line);
 	if (!ft_is_valid_float(&line))
 		return (false);
@@ -23,7 +26,21 @@ bool	ft_check_ambient(char *line)
 	return (true);
 }
 
-bool	ft_check_camera(char *line)
+bool	ft_check_plane(char *line)
+{
+	ft_skip_space(&line);
+	if (!ft_is_valid_float_block(&line))
+		return (false);
+	ft_skip_space(&line);
+	if (!ft_is_valid_float_block(&line))
+		return (false);
+	ft_skip_space(&line);
+	if (!ft_is_valid_rgb_block(&line))
+		return (false);
+	return (true);
+}
+
+bool	ft_check_cylinder(char *line)
 {
 	ft_skip_space(&line);
 	if (!ft_is_valid_float_block(&line))
@@ -33,14 +50,6 @@ bool	ft_check_camera(char *line)
 		return (false);
 	ft_skip_space(&line);
 	if (!ft_is_valid_float(&line))
-		return (false);
-	return (true);
-}
-
-bool	ft_check_light(char *line)
-{
-	ft_skip_space(&line);
-	if (!ft_is_valid_float_block(&line))
 		return (false);
 	ft_skip_space(&line);
 	if (!ft_is_valid_float(&line))
