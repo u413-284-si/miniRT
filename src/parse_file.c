5956cbdf584b837d89e712bced06e5e0cf9e6423
Gabriel Wolf <gwolf@student.42vienna.com>
2023-11-13 12:41:39 +0100

    feat: finish ft parse_lines

--- src/parse_file.c
+++ src/parse_file.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 16:02:54 by gwolf             #+#    #+#             */
-/*   Updated: 2023/11/12 17:50:58 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/11/13 12:36:09 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ t_err	ft_check_file(char *filename, char ***lines, int entity_count[6]);
 
 t_err	ft_malloc_entities(t_entities *entities)
 {
-	if (ft_err_malloc((void **)entities->lsrc, sizeof(t_light) * entities->lsrc_count))
+	if (ft_err_malloc((void **)&entities->lsrc, sizeof(t_light) * entities->lsrc_count))
 		return (ERROR);
-	if (ft_err_malloc((void **)entities->obj, sizeof(t_shape) * entities->total))
+	if (ft_err_malloc((void **)&entities->obj, sizeof(t_shape) * entities->total))
 	{
 		free(entities->lsrc);
 		return (ERROR);
@@ -28,25 +28,30 @@ t_err	ft_malloc_entities(t_entities *entities)
 
 void	ft_parse_lines(t_entities *entities, char **lines)
 {
-	int	i;
-	int	j;
+	size_t	lights;
+	size_t	id;
 
-	i = 0;
-	j = 0;
+	lights = 0;
+	id = 0;
 	while (*lines)
 	{
-		if (!ft_strncmp(*lines, "A ", 2))
+		if (**lines == 'A')
 			ft_parse_ambient(*lines + 2, &entities->ambient);
-		else if (!ft_strncmp(*lines, "C ", 2))
+		else if (**lines == 'C')
 			ft_parse_camera(*lines + 2, NULL);
-		else if (!ft_strncmp(*lines, "L ", 2))
-			ft_parse_light(*lines + 2, NULL);
-		else if (!ft_strncmp(*lines, "sp ", 3))
-			ft_parse_sphere(*lines + 3);
-		else if (!ft_strncmp(*lines, "pl ", 3))
-			ft_parse_plane(*lines + 3);
-		else if (!ft_strncmp(*lines, "cy ", 3))
-			ft_parse_cylinder(*lines + 3);
+		else if (**lines == 'L')
+			ft_parse_light(*lines + 2, &entities->lsrc[lights++]);
+		else
+		{
+			if (**lines == 's')
+				ft_parse_sphere(*lines + 3, &entities->obj[id], id);
+			else if (**lines == 'p')
+				ft_parse_plane(*lines + 3, &entities->obj[id], id);
+			else if (**lines == 'c')
+				ft_parse_cylinder(*lines + 3, &entities->obj[id], id);
+			id++;
+		}
+		lines++;
 	}
 }
 
