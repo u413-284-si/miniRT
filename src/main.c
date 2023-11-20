ac833b98aa6d90cd7340c06ea4a6b3e4bde09707
Gabriel <gwolf@student.42vienna.com>
2023-11-20 11:01:02 +0100

    fix: leave if paring error

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 14:55:47 by sqiu              #+#    #+#             */
-/*   Updated: 2023/11/20 09:55:02 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/11/20 10:51:48 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,8 @@ int	main(int argc, char **argv)
 		ft_putendl_fd("Error\nUsage: ./miniRT file.rt", 2);
 		exit(1);
 	}
-	ft_parse_file(argv[1], &scene, &cam);
+	if (ft_parse_file(argv[1], &scene, &cam))
+		exit(1);
 	ft_initiate_image(&image);
 	ft_initiate_camera(&cam);
 	ft_initiate_viewport(&viewport, cam, image);
