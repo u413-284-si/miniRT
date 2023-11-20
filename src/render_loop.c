48bed4dc1b116854ec6be6273f06fbf8f83dd3d3
Gabriel <gwolf@student.42vienna.com>
2023-11-20 16:43:26 +0100

    fix: free mlx if loop ends

--- src/render_loop.c
+++ src/render_loop.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 15:07:00 by gwolf             #+#    #+#             */
-/*   Updated: 2023/11/20 16:14:29 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/11/20 16:24:22 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,5 @@ void	ft_start_mlx_loop(t_render *render)
 	mlx_hook(render->win_ptr, KeyPress, KeyPressMask, ft_key_hook_press, render);
 	mlx_hook(render->win_ptr, DestroyNotify, StructureNotifyMask, ft_end_loop, render);
 	mlx_loop(render->mlx_ptr);
+	ft_free_mlx(render->mlx_ptr, render->win_ptr, render->buffer.ptr);
 }
