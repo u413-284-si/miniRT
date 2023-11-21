9e495487f726d024164769850eb22e47aae0a2ac
Gabriel Wolf <gwolf@student.42vienna.com>
2023-11-21 18:45:16 +0100

    fix: rename ft keyhook_press

--- src/render_keyhook.c
+++ src/render_keyhook.c
@@ -6,13 +6,13 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 15:08:26 by gwolf             #+#    #+#             */
-/*   Updated: 2023/11/20 15:24:01 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/11/21 08:27:53 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "render.h"
 
-int	ft_key_hook_press(int key, t_render *render)
+int	ft_keyhook_press(int key, t_render *render)
 {
 	if (key == KEY_ESC)
 		mlx_loop_end(render->mlx_ptr);
