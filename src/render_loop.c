c86e540840982512d76df1b78accb269ac42d73f
Gabriel Wolf <gwolf@student.42vienna.com>
2023-11-21 18:49:53 +0100

    fix: swap ptr to addr

--- src/render_loop.c
+++ src/render_loop.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 15:07:00 by gwolf             #+#    #+#             */
-/*   Updated: 2023/11/21 18:43:48 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/11/21 18:49:29 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,6 @@ void	ft_start_mlx_loop(t_render *render, t_image image)
 	mlx_hook(render->win_ptr, KeyPress, KeyPressMask, ft_keyhook_press, render);
 	mlx_hook(render->win_ptr, DestroyNotify, StructureNotifyMask, ft_end_loop, render);
 	mlx_loop(render->mlx_ptr);
-	ft_print_image_as_ppm((int *)render->buffer.ptr, image.image_width, image.image_height);
+	ft_print_image_as_ppm((int *)render->buffer.addr, image.image_width, image.image_height);
 	ft_free_mlx(render->mlx_ptr, render->win_ptr, render->buffer.ptr);
 }
