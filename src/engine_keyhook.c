2fa2593f91d99677bb605c9a7177d9d3f63cebb0
Gabriel Wolf <gwolf@student.42vienna.com>
2023-11-26 11:22:39 +0100

    feat: first drft of obj manip

--- src/engine_keyhook.c
+++ src/engine_keyhook.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 15:08:26 by gwolf             #+#    #+#             */
-/*   Updated: 2023/11/26 10:18:19 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/11/26 11:18:43 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,51 @@ void	ft_move_camera(int key, t_cam *cam, t_viewport *vp, t_image *image)
 	ft_initiate_viewport(vp, *cam, *image);
 }
 
+void	ft_change_active_hittable(int key, t_entities *scene)
+{
+	if (scene->active == -1)
+		return ;
+	if (key == KEY_ARROW_LEFT)
+	{
+		scene->active--;
+		if (scene->active < 0)
+			scene->active = scene->total - 1;
+	}
+	if (key == KEY_ARROW_RIGHT)
+	{
+		scene->active++;
+		if (scene->active == scene->total)
+			scene->active = 0;
+	}
+}
+
+
+void	ft_manip_sphere(int key, t_sphere *sp)
+{
+	if (key == KEY_Z)
+		sp->centre.z -= 1;
+	else if (key == KEY_I)
+		sp->centre.z += 1;
+	if (key == KEY_U)
+		sp->centre.y -= 1;
+	else if (key == KEY_J)
+		sp->centre.y += 1;
+	if (key == KEY_H)
+		sp->centre.x -= 1;
+	else if (key == KEY_K)
+		sp->centre.x += 1;
+	if (key == KEY_N)
+		sp->r -= 1;
+	else if (key == KEY_M)
+		sp->r += 1;
+}
+
+void	ft_manip_hittable(int key, t_entities *scene)
+{
+	if (scene->obj[scene->active].type == SPHERE)
+		ft_manip_sphere(key, &scene->obj[scene->active].params.sp);
+}
+
 int	ft_keyhook_press(int key, t_engine *engine)
 {
 	static bool	print;
@@ -46,5 +91,10 @@ int	ft_keyhook_press(int key, t_engine *engine)
 	{
 		ft_move_camera(key, &engine->cam, &engine->vp, &engine->image);
 	}
+	else if (key == KEY_ARROW_LEFT || key == KEY_ARROW_RIGHT)
+		ft_change_active_hittable(key, &engine->scene);
+	else if (key == KEY_Z || key == KEY_U || key == KEY_I || key == KEY_H
+		|| key == KEY_J || key == KEY_K || key == KEY_N || key == KEY_M)
+		ft_manip_hittable(key, &engine->scene);
 	return (0);
 }
