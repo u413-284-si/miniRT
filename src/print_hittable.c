23f9c2195c06183ea07c7116d59062edb4a77476
Gabriel <gwolf@student.42vienna.com>
2023-12-18 13:44:10 +0100

    fix: inc precision

--- src/print_hittable.c
+++ src/print_hittable.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/17 10:16:27 by gwolf             #+#    #+#             */
-/*   Updated: 2023/12/17 10:53:47 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/12/18 13:44:01 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 void	ft_print_vec3(const t_vec3 vec)
 {
-	printf("x: %.2f, y: %.2f, z: %.2f\n", vec.x, vec.y, vec.z);
+	printf("x: %.6f, y: %.6f, z: %.6f\n", vec.x, vec.y, vec.z);
 }
 
 void	ft_print_colour(const t_colour col)
 {
-	printf("r: %.2f, g: %.2f, b: %.2f\n", col.r, col.g, col.b);
+	printf("r: %.6f, g: %.6f, b: %.6f\n", col.r, col.g, col.b);
 }
 
 void	ft_print_sphere(const t_sphere sp)
