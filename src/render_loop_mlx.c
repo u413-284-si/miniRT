120844748f81ad1d12d9531fcd5ad5b762ac2be5
Gabriel Wolf <gwolf@student.42vienna.com>
2023-12-22 00:49:08 +0100

    fix: del ft_end_loop, call mlx ft directly

--- src/render_loop_mlx.c
+++ src/render_loop_mlx.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 22:37:57 by gwolf             #+#    #+#             */
-/*   Updated: 2023/12/22 00:18:56 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/12/22 00:48:14 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,11 @@ int	ft_draw_scene(t_render *render)
 	return (0);
 }
 
-int	ft_end_loop(void *mlx_ptr)
-{
-	mlx_loop_end(mlx_ptr);
-	return (0);
-}
-
 void	ft_render_start_loop(t_render *render)
 {
 	mlx_hook(render->mlx_ptrs.win_ptr, KeyPress, KeyPressMask, ft_keyhook_press, render);
 	mlx_hook(render->mlx_ptrs.win_ptr, DestroyNotify, StructureNotifyMask,
-		ft_end_loop, render->mlx_ptrs.mlx_ptr);
+		mlx_loop_end, render->mlx_ptrs.mlx_ptr);
 	mlx_loop_hook(render->mlx_ptrs.mlx_ptr, ft_draw_scene, render);
 	mlx_loop(render->mlx_ptrs.mlx_ptr);
 	ft_free_mlx(render->mlx_ptrs.mlx_ptr, render->mlx_ptrs.win_ptr, render->mlx_ptrs.img.ptr);
