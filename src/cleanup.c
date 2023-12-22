466c5a5017a866f58b1fd6a279ff0a6ed7cc8efa
Gabriel <gwolf@student.42vienna.com>
2023-12-22 14:11:02 +0100

    fix: norminette

--- src/cleanup.c
+++ src/cleanup.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 15:59:11 by gwolf             #+#    #+#             */
-/*   Updated: 2023/12/22 14:04:42 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/12/22 14:10:52 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,8 +33,8 @@ void	ft_free_mlx(void *mlx_ptr, void *win_ptr, void *img_ptr)
 			mlx_destroy_image(mlx_ptr, img_ptr);
 		if (win_ptr)
 			mlx_destroy_window(mlx_ptr, win_ptr);
-	mlx_destroy_display(mlx_ptr);
-	free(mlx_ptr);
+		mlx_destroy_display(mlx_ptr);
+		free(mlx_ptr);
 	}
 }
 
