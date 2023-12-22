e1dfbe05facc5d02926f53ee7e1c5e45cf3a9607
Gabriel <gwolf@student.42vienna.com>
2023-12-22 14:05:38 +0100

    fix: change cleanup to check for mlx

--- src/cleanup.c
+++ src/cleanup.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 15:59:11 by gwolf             #+#    #+#             */
-/*   Updated: 2023/12/19 22:33:19 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/12/22 14:04:42 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,14 +27,14 @@ void	ft_free_char_arr(char **array)
 
 void	ft_free_mlx(void *mlx_ptr, void *win_ptr, void *img_ptr)
 {
-	if (img_ptr)
-		mlx_destroy_image(mlx_ptr, img_ptr);
-	if (win_ptr)
-		mlx_destroy_window(mlx_ptr, win_ptr);
 	if (mlx_ptr)
 	{
-		mlx_destroy_display(mlx_ptr);
-		free(mlx_ptr);
+		if (img_ptr)
+			mlx_destroy_image(mlx_ptr, img_ptr);
+		if (win_ptr)
+			mlx_destroy_window(mlx_ptr, win_ptr);
+	mlx_destroy_display(mlx_ptr);
+	free(mlx_ptr);
 	}
 }
 
