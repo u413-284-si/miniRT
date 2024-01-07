bcd480558e5f75098b0b5205209f9afdd5dd57d5
Gabriel <gwolf@student.42vienna.com>
2024-01-07 17:58:53 +0100

    fix: finish merge

--- src/render_compose_image_bonus.c
+++ src/render_compose_image_bonus.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/07 14:22:40 by gwolf             #+#    #+#             */
-/*   Updated: 2024/01/07 14:22:53 by gwolf            ###   ########.fr       */
+/*   Updated: 2024/01/07 17:58:03 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,28 @@
 int	ft_draw_scene(t_render *render)
 {
 	const uint64_t	starttime = ft_get_timeofday_ms();
-	const uint32_t	fps = 1000 / (starttime - render->last_render_time);
+	//const uint32_t	fps = 1000 / (starttime - render->last_render_time);
 
-	ft_render_image(render);
+	if (render->is_printing)
+	{
+		mlx_string_put(render->mlx_ptrs.mlx_ptr, render->mlx_ptrs.win_ptr,
+			200, 20, render->menu.font_col, "Printing - Please wait");
+		ft_output_as_ppm(render->mlx_ptrs.img, &render->is_printing);
+		mlx_do_key_autorepeaton(render->mlx_ptrs.mlx_ptr);
+		return (0);
+	}
+	if (render->is_changed)
+	{
+		ft_render_image(render);
+		mlx_put_image_to_window(render->mlx_ptrs.mlx_ptr,
+			render->mlx_ptrs.win_ptr, render->mlx_ptrs.img.ptr, 0, 0);
+		ft_blend_background(&render->mlx_ptrs.img, &render->mlx_ptrs.veil,
+			render->menu);
+		render->is_changed = false;
+	}
+	ft_menu_put_text(render);
 	printf("MS: %lu\n", starttime - render->last_render_time);
-	printf("FPS: %u\n", fps);
+	//printf("FPS: %u\n", fps);
 	render->last_render_time = starttime;
 	return (0);
 }
--- src/render_loop_mlx.c
+++ src/render_loop_mlx.c
@@ -6,34 +6,11 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 22:37:57 by gwolf             #+#    #+#             */
-/*   Updated: 2024/01/07 17:55:02 by gwolf            ###   ########.fr       */
+/*   Updated: 2024/01/07 17:55:59 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "render.h"
-
-int	ft_draw_scene(t_render *render)
-{
-	if (render->is_printing)
-	{
-		mlx_string_put(render->mlx_ptrs.mlx_ptr, render->mlx_ptrs.win_ptr,
-			200, 20, render->menu.font_col, "Printing - Please wait");
-		ft_output_as_ppm(render->mlx_ptrs.img, &render->is_printing);
-		mlx_do_key_autorepeaton(render->mlx_ptrs.mlx_ptr);
-		return (0);
-	}
-	if (render->is_changed)
-	{
-		ft_render_image(render);
-		mlx_put_image_to_window(render->mlx_ptrs.mlx_ptr,
-			render->mlx_ptrs.win_ptr, render->mlx_ptrs.img.ptr, 0, 0);
-		ft_blend_background(&render->mlx_ptrs.img, &render->mlx_ptrs.veil,
-			render->menu);
-		render->is_changed = false;
-	}
-	ft_menu_put_text(render);
-	return (0);
-}
 
 void	ft_render_start_loop(t_render *render)
 {
