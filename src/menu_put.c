daf9602b17478493b6f1bb554d0e5222174bfb1a
Gabriel <gwolf@student.42vienna.com>
2024-01-07 18:19:35 +0100

    fix: get rid of empty lines

--- src/menu_put.c
+++ src/menu_put.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/06 12:35:59 by gwolf             #+#    #+#             */
-/*   Updated: 2024/01/06 19:09:57 by gwolf            ###   ########.fr       */
+/*   Updated: 2024/01/07 18:05:16 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,17 +25,15 @@ void	ft_menu_put_text(t_render *render)
 		render->mlx_ptrs.win_ptr, render->mlx_ptrs.veil.ptr, 0, 0);
 	put.pos = ft_put_mode(put, render->mode);
 	if (render->menu.cur_page % 3 == 0)
-		ft_put_page_scene(put,
-			render->scene.obj[render->active_hittable], render->menu.cur_page);
+		ft_put_page_scene(put, render->scene.obj[render->active_hittable],
+			render->menu.cur_page);
 	else if (render->menu.cur_page % 3 == 1 && render->active_light == 0)
-		ft_put_page_ambient(put,
-			render->scene.ambient, render->menu.cur_page);
+		ft_put_page_ambient(put, render->scene.ambient, render->menu.cur_page);
 	else if (render->menu.cur_page % 3 == 1)
 		ft_put_page_light(put, render->scene.lsrc[render->active_light - 1],
 			render->menu.cur_page);
 	else if (render->menu.cur_page % 3 == 2)
-		ft_put_page_cam(put,
-			render->cam, render->menu.cur_page);
+		ft_put_page_cam(put, render->cam, render->menu.cur_page);
 	put.pos = (t_vec2i){20, 490};
 	ft_put_info(put);
 }
