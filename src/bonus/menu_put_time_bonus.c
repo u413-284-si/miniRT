7fee1ac2e3a6a62100c729547e728205103bdcd3
Gabriel Wolf <gwolf@student.42vienna.com>
2024-01-09 16:24:25 +0100

    fix: algin time/fps

--- src/bonus/menu_put_time_bonus.c
+++ src/bonus/menu_put_time_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   menu_put_fps_bonus.c                               :+:      :+:    :+:   */
+/*   menu_put_time_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 10:46:17 by gwolf             #+#    #+#             */
-/*   Updated: 2024/01/08 11:02:45 by gwolf            ###   ########.fr       */
+/*   Updated: 2024/01/09 16:14:44 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,16 @@ t_vec2i	ft_put_time(t_putinfo put, uint64_t last_render_time, bool show_fps)
 	if (show_fps == true)
 	{
 		ft_put_str(put, "FPS: ");
-		put.pos.x += 50;
+		put.pos.x += 30;
 		ft_put_float(put, (t_numinfo){.numf = fps, .pad = 2, .prec = 2});
-		put.pos.x -= 50;
+		put.pos.x -= 30;
 		put.pos.y += Y_NEXT_LINE_BIG;
 		return (put.pos);
 	}
 	ft_put_str(put, "Time (ms): ");
-	put.pos.x += 110;
+	put.pos.x += 90;
 	ft_put_int(put, (t_numinfo){.numi = last_render_time, .pad = 4, .prec = 0});
-	put.pos.x -= 110;
+	put.pos.x -= 90;
 	put.pos.y += Y_NEXT_LINE_BIG;
 	return (put.pos);
 }
