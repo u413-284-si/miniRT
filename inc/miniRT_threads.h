210ed1bd6bef4a6beeced258796bd116dafec612
Gabriel Wolf <gwolf@student.42vienna.com>
2024-01-14 10:25:03 +0100

    feat: generic thread generation

---
+++ inc/miniRT_threads.h
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   miniRT_threads.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/13 16:34:50 by gwolf             #+#    #+#             */
+/*   Updated: 2024/01/14 10:14:25 by gwolf            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINIRT_THREADS_H
+# define MINIRT_THREADS_H
+
+/* ====== LIBRARIES ====== */
+
+# include <pthread.h>
+# include "error_type.h"
+# include "error_msg.h"
+
+/* ====== MACROS ====== */
+
+# define NUM_THREADS 16
+
+/* ====== STRUCTS ====== */
+
+typedef struct s_thread_data
+{
+	int32_t		id;
+	pthread_t	t_id;
+	void		*arg;
+}	t_thread_data;
+
+/* ====== FUNCTIONS ====== */
+
+/**
+ * @brief Creates NUM_THREADS threads and joins them.
+ *
+ * Routine can be any function which takes a void pointer as argument and
+ * returns a void pointer.
+ * @param arg		Argument to pass to routine
+ * @param routine	Function to call
+ * @return t_err	ERROR, if thread_create() fails or SUCCESS
+ */
+t_err	ft_spin_threads(void *arg, void *(*routine)(void *));
+
+/**
+ * @brief Creates NUM_THREADS threads and calls routine with arg.
+ *
+ * Sets every thread's id to -1. This way, ft_join_threads() can be called
+ * with join_all = false to join only the threads which have been created.
+ * If thread_create() fails, sets errno, ft_perror()s the error message and
+ * returns ERROR.
+ * @param arg		Argument to pass to routine
+ * @param routine	Function to call
+ * @param threads	Array of t_thread_data
+ * @return t_err	ERROR, if thread_create() fails or SUCCESS
+ */
+t_err	ft_create_threads(void *arg, void *(*routine)(void *),
+			t_thread_data threads[NUM_THREADS]);
+
+/**
+ * @brief Joins threads.
+ *
+ * If join_all is true, joins all threads. If join_all is false, joins only
+ * the threads which have been created.
+ * @param threads	Array of t_thread_data
+ * @param join_all	Whether to join all threads or only the ones which have
+ * 					been created
+ */
+void	ft_join_threads(t_thread_data threads[NUM_THREADS], bool join_all);
+
+#endif
---
+++ src/threads.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   threads.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/13 16:28:01 by gwolf             #+#    #+#             */
+/*   Updated: 2024/01/14 10:21:08 by gwolf            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "miniRT_threads.h"
+
+t_err	ft_create_threads(void *arg, void *(*routine)(void *),
+			t_thread_data threads[NUM_THREADS])
+{
+	int	i;
+
+	i = -1;
+	while (++i < NUM_THREADS)
+		threads[i].id = -1;
+	i = -1;
+	while (++i < NUM_THREADS)
+	{
+		threads[i].id = i;
+		threads[i].arg = arg;
+		errno = pthread_create(&threads[i].t_id, NULL, routine, &threads[i]);
+		if (errno)
+		{
+			ft_perror("pthread_create() failed", errno);
+			return (ERROR);
+		}
+	}
+	return (SUCCESS);
+}
+
+void	ft_join_threads(t_thread_data threads[NUM_THREADS], bool join_all)
+{
+	int	i;
+
+	i = -1;
+	if (join_all)
+	{
+		while (++i < NUM_THREADS)
+		{
+			errno = pthread_join(threads[i].t_id, NULL);
+			if (errno)
+				ft_perror("pthread_join() failed", errno);
+		}
+		return ;
+	}
+	while (++i < NUM_THREADS)
+	{
+		if (threads[i].id == -1)
+			continue ;
+		errno = pthread_join(threads[i].t_id, NULL);
+		if (errno)
+			ft_perror("pthread_join() failed", errno);
+	}
+}
+
+t_err	ft_spin_threads(void *arg, void *(*routine)(void *))
+{
+	t_thread_data	threads[NUM_THREADS];
+
+	if (ft_create_threads(arg, routine, threads))
+	{
+		ft_join_threads(threads, false);
+		return (ERROR);
+	}
+	ft_join_threads(threads, true);
+	return (SUCCESS);
+}
