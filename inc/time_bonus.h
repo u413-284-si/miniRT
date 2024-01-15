52d773efe35f06a777d88b98a38a1a584462caa3
Gabriel Wolf <gwolf@student.42vienna.com>
2024-01-15 22:21:59 +0100

    fix: add doxygen

--- inc/time_bonus.h
+++ inc/time_bonus.h
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/07 13:22:36 by gwolf             #+#    #+#             */
-/*   Updated: 2024/01/08 10:25:45 by gwolf            ###   ########.fr       */
+/*   Updated: 2024/01/15 22:21:47 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,12 @@
 
 /* ====== FUNCTIONS ====== */
 
+/**
+ * @brief Returns the current time in milliseconds.
+ *
+ * Uses clock_gettime() for high precision.
+ * @return uint64_t Timestamp in milliseconds.
+ */
 uint64_t	ft_get_time_ms(void);
 
 #endif
