26d495b4a71d5657a4761fecd3b24fca86192f2e
sirui <sirui.qiu01@gmail.com>
2024-01-17 00:48:13 +0100

    perf: reduce dot product calculations in cone

--- src/hit_cone_bonus.c
+++ src/hit_cone_bonus.c
@@ -6,7 +6,7 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/31 11:33:09 by sqiu              #+#    #+#             */
-/*   Updated: 2024/01/17 00:22:50 by sqiu             ###   ########.fr       */
+/*   Updated: 2024/01/17 00:46:13 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,15 +39,19 @@ bool	ft_co_calc_pot_hits(t_cone co, t_ray ray, float potential_hits[3])
 {
 	t_vec3		ray_apex;
 	t_equation	eq;
+	float		dot_product_ray_axis;
+	float		dot_product_apex_axis;
+	float		cos_2_angle;
 
 	ray_apex = ft_vec3_sub(ray.origin, co.apex);
-	eq.a = pow(ft_vec3_dot(ray.direction, co.axis), 2) \
-		- pow(cos(co.angle), 2);
-	eq.b = 2.0 * (ft_vec3_dot(ray.direction, co.axis) * \
-		ft_vec3_dot(ray_apex, co.axis) - ft_vec3_dot(ray.direction, \
-		ray_apex) * pow(cos(co.angle), 2));
-	eq.c = pow(ft_vec3_dot(ray_apex, co.axis), 2) - ft_vec3_dot(ray_apex, \
-		ray_apex) * pow(cos(co.angle), 2);
+	dot_product_ray_axis = ft_vec3_dot(ray.direction, co.axis);
+	dot_product_apex_axis = ft_vec3_dot(ray_apex, co.axis);
+	cos_2_angle = pow(cos(co.angle), 2);
+	eq.a = pow(dot_product_ray_axis, 2) - cos_2_angle;
+	eq.b = 2.0 * (dot_product_ray_axis * dot_product_apex_axis - \
+		ft_vec3_dot(ray.direction, ray_apex) * cos_2_angle);
+	eq.c = pow(dot_product_apex_axis, 2) - ft_vec3_dot(ray_apex, ray_apex) \
+		* cos_2_angle;
 	if (!ft_solve(&eq))
 		return (false);
 	potential_hits[0] = eq.d1;
