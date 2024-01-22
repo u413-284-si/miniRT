860d9f24d634855ac7bdea6cce99aa66380a0718
Gabriel <gwolf@student.42vienna.com>
2024-01-22 17:16:39 +0100

    fix: better max for screen size

--- src/check_win_size_bonus.c
+++ src/check_win_size_bonus.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 14:04:43 by gwolf             #+#    #+#             */
-/*   Updated: 2024/01/22 15:11:33 by gwolf            ###   ########.fr       */
+/*   Updated: 2024/01/22 17:16:27 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 t_type	ft_check_win_size(char *line)
 {
 	ft_rm_space(&line);
-	if (!ft_isvalid_int(&line, WIN_SIZE_X, 2000, true))
+	if (!ft_isvalid_int(&line, WIN_SIZE_X, 1920, true))
 		return (UNKNOWN);
 	line++;
-	if (!ft_isvalid_int(&line, WIN_SIZE_Y, 2000, false))
+	if (!ft_isvalid_int(&line, WIN_SIZE_Y, 1080, false))
 		return (UNKNOWN);
 	ft_rm_space(&line);
 	if (*line != '\0')
