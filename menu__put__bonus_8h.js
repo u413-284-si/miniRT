747aba958da677b90949838f8a53344dcd836a99
u413-284-si <sirui.qiu01@gmail.com>
2024-01-26 11:22:21 +0000

    Deploying to gh-pages from @ u413-284-si/miniRT@ae6335671083b656884a799a8613afd3c104a7c6 🚀

--- menu__put__bonus_8h.js
+++ menu__put__bonus_8h.js
@@ -1,5 +1,6 @@
 var menu__put__bonus_8h =
 [
     [ "ft_menu_put_options", "menu__put__bonus_8h.html#a364e2b6623c434fb926063c5e75699fa", null ],
+    [ "ft_put_co", "menu__put__bonus_8h.html#af86e62ade8b462c44092a160d876544e", null ],
     [ "ft_put_time", "menu__put__bonus_8h.html#a8f946374b776bc149c5bc03048f6ed7f", null ]
 ];
