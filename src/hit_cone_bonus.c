2bc6f8aa5ba3c4a91dfa9b1f3d4053232a56f2f8
Sirui Qiu <sqiu@c2r2p2.42vienna.com>
2024-01-26 14:14:12 +0100

    hotfix: initialise equation

--- src/hit_cone_bonus.c
+++ src/hit_cone_bonus.c
@@ -6,7 +6,7 @@
 /*   By: sqiu <sqiu@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/31 11:33:09 by sqiu              #+#    #+#             */
-/*   Updated: 2024/01/23 15:29:07 by sqiu             ###   ########.fr       */
+/*   Updated: 2024/01/26 13:58:09 by sqiu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,7 @@ bool	ft_co_calc_pot_hits(t_cone co, t_ray ray, float potential_hits[3])
 	float		dot_product_apex_axis;
 	float		cos_2_angle;
 
+	eq = (t_equation){0};
 	ray_apex = ft_vec3_sub(ray.origin, co.apex);
 	dot_product_ray_axis = ft_vec3_dot(ray.direction, co.axis);
 	dot_product_apex_axis = ft_vec3_dot(ray_apex, co.axis);
